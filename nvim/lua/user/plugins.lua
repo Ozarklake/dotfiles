a478116dda129e2e6da8189242f7c66de4096565
hy <hang.yao.hy@gmail.com>
2022-12-26 10:54:36 +0800

    use dracula theme

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -91,8 +91,9 @@ return packer.startup(function(use)
   use "windwp/nvim-autopairs"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
-  --
   use "feline-nvim/feline.nvim"
+
+  use "Mofiqul/dracula.nvim"
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
