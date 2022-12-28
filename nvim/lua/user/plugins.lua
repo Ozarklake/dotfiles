ebb4b6ae2cf5657337579673d33ec9e1b2e8b9e9
Hy <hang.yao.hy@gmail.com>
2022-12-28 18:08:27 +0800

    Disable url jumping of kitty

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -96,7 +96,7 @@ return packer.startup(function(use)
   use {
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons',
-    cmd=":NvimTreeToggle",
+    cmd="NvimTreeToggle",
     config = function()
       require("user.nvim-tree")
     end
