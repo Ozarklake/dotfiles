78a3b7b0d1c296ae7393ac4da96e155cabaef554
Hy <hang.yao.hy@gmail.com>
2022-12-29 12:27:55 +0800

    Add .krew ot the PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,12 @@
 if status is-interactive
     alias vim="nvim";
+    alias k="kubectl";
     export TERM="xterm-256color";
+    set PATH $PATH ~/.krew/bin
+
     # Commands to run in interactive sessions can go here
 end
 
+# THEME PURE #
+set fish_function_path /Users/yh/.config/fish/functions/theme-pure/functions/ $fish_function_path
+source /Users/yh/.config/fish/functions/theme-pure/conf.d/pure.fish
